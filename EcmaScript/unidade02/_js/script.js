2124463359291533d9ff2ab20f248af5e7825936
Samuel Guibor <141835578+SamuelGuibor@users.noreply.github.com>
2024-09-13 17:10:54 -0300

    Add files via upload

---
+++ EcmaScript/unidade02/_js/script.js
@@ -0,0 +1,8 @@
+/*
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.29/browser.min.js"></script>
+*/
+
+var criarDeNome = function(primeiroNome="Matheus", ultimoNome="Alves") {
+    console.log(primeiroNome + " " + ultimoNome);  
+};
+criarDeNome();  
